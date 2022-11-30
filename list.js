6dbe551c885f334d84acf6417743f20ee5721d0a
Sam Fleming <flemo1485@gmail.com>
2022-11-30 18:36:12 +1000

    unchecked formatted

--- list.js
+++ list.js
@@ -6,4 +6,30 @@ export class List {
         this.list_name = list_name;
         this.size = size;
     }
+
+    CheckedStrings(){
+        checked_string = ''
+        unchecked_string = ''
+        for (let i = 1; i< this.checked.length; i++){
+            if(i = this.checked.length){
+                checked_string += this.checked[i] + "."
+            }else{
+                checked_string += this.checked[i] + ", "
+            }
+        }
+        const unchecked_length = this.size - this.checked.length
+
+        for (let j = 1; j<= this.size; j++){
+            if(!this.checked.includes(j)){
+                unchecked_string += j + ", "
+            }
+            
+        }
+        unchecked_string = unchecked_string.slice(0, -2)
+        unchecked_string += "."
+       
+
+        return [checked_string, unchecked_string]
+    }
+
   }
